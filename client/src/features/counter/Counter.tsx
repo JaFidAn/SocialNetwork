239d9972efc952e399d80408f182a25641fc74c7
Jafidan <r.alagezov@gmail.com>
2025-02-24 01:07:54 +0400

    End of Section 9

---
+++ client/src/features/counter/Counter.tsx
@@ -0,0 +1,40 @@
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useStore } from "../../lib/hooks/useStore";
+import { observer } from "mobx-react-lite";
+
+const Counter = observer(function Counter() {
+  const { counterStore } = useStore();
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        {counterStore.title}
+      </Typography>
+      <Typography variant="h5">The Count is: {counterStore.count}</Typography>
+      <ButtonGroup sx={{ mt: 3 }}>
+        <Button
+          onClick={() => counterStore.decrement()}
+          variant="contained"
+          color="error"
+        >
+          Decrement
+        </Button>
+        <Button
+          onClick={() => counterStore.increment()}
+          variant="contained"
+          color="success"
+        >
+          Increment
+        </Button>
+        <Button
+          onClick={() => counterStore.increment(5)}
+          variant="contained"
+          color="primary"
+        >
+          Increment by 5
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+});
+
+export default Counter;
